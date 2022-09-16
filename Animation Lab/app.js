07c97cb2f02959c8f6819ed6e7c84440250316fa
devant6 <devant6@hotmail.com>
2022-09-15 22:55:24 -0400

    Update app.js

--- Animation Lab/app.js
+++ Animation Lab/app.js
@@ -1,19 +1,23 @@
-var items = document.getElementsByClassName("item");
 
+class Test{
+    constructor(){
+        let items = document.getElementsByClassName("item");
+        for(const item of items){
+            item.addEventListener("mouseover", (event)=>{
+                event.target.style.backgroundColor = "pink";
+            })
 
-items.addEventListener("mouseover", (event)=>{
-    
-})
-
-items.addEventListener("mouseout", (event)=>{
-
-})
-
-items.addEventListener("click", (event)=>{
-    console.log("click")
-    for(let i = 0; i < navItems.length; i++) {
-        gsap.to(navItems[i], 
-        { duration: .7, delay: i, alpha: 0, x: -1000 } 
-        );
+            item.addEventListener("mouseout", (event)=>{
+                event.target.style.backgroundColor = "rgb(174, 44, 176)";
+            })
+        
+            item.addEventListener("click", (event)=>{
+                    gsap.to(item, 
+                    { duration: .7, alpha: 0, x: -1000 } 
+                    );
+                })
+            
+        }
     }
-})
+}
+let t = new Test();

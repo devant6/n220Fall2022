657850a1d9dcb46327a31f982ec3359a1abb8c48
devant6 <devant6@hotmail.com>
2022-09-20 14:34:06 -0400

    Initial Commit

--- reactintrolab/my-app/src/App.js
+++ reactintrolab/my-app/src/App.js
@@ -1,11 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function App() { //npx create-react-app my-app
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" /> 
+        <img src={logo} className="App-log" alt="logo" /> 
         <p>
           Hey
         </p>
